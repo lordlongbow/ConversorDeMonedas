325f247532270dbd1bf8d7d05c449b5a52b0939a
lordlongbow <102767169+lordlongbow@users.noreply.github.com>
2023-03-27 04:17:33 -0300

    Primera entrega
    Funcionalidades completas, Validaciones se podrian mejorar, pero el tiempo aprieta.

---
+++ app/src/main/java/com/example/conversordemoneda/MainActivity.java
@@ -0,0 +1,68 @@
+package com.example.conversordemoneda;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    //recupero los componentes con los que voy a trabajar
+    private MainActivityViewModel mv;
+    private EditText etDollar, etEuro, etConvertido;
+    private Button btConvertir;
+    private RadioButton rbEuroDollar, rbDollarEuro;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //Creo el viewModel
+        mv = ViewModelProvider.AndroidViewModelFactory.getInstance(getApplication()).create(MainActivityViewModel.class);
+        //Seteo las variables de los componentes con sus componentes ID
+        //variables y componentes EditText
+        this.etDollar = findViewById(R.id.etDolares);
+        this.etEuro = findViewById(R.id.etEuros);
+        this.etConvertido = findViewById(R.id.etCambio);
+        //Variables y Componentes Radio Buttons
+        this.rbDollarEuro = findViewById(R.id.rBDolarEuro);
+        this.rbEuroDollar = findViewById(R.id.rBEuroDolar);
+        //Variable y Componentes Boton
+        this.btConvertir = findViewById(R.id.btnConvertir);
+
+        //realizo un Observer sobre el valor del convertido en el viewModel
+        mv.getConvertido().observe(this, new Observer<Double>() {
+            @Override
+            public void onChanged(Double aDouble) {
+                etConvertido.setText(aDouble + "");
+            }
+        });
+
+
+        //Escuchar al boton convertir
+        btConvertir.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(rbDollarEuro.isChecked()==true){
+                  
+                    mv.convertirDollarEuro(etDollar.getText().toString());
+                }else if(rbEuroDollar.isChecked()==true){
+
+                    mv.convertirEuroDollar(etEuro.getText().toString());
+                }else {
+                    Toast.makeText(getApplicationContext(), "Debe seleccionar una opcion", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+    }
+}
---
+++ app/src/main/java/com/example/conversordemoneda/MainActivityViewModel.java
@@ -0,0 +1,55 @@
+package com.example.conversordemoneda;
+
+import android.app.Application;
+import android.content.Context;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+
+public class MainActivityViewModel extends AndroidViewModel {
+
+    private Context contexto;
+    private MutableLiveData<Double> Convertido =  null;
+
+    private String mensaje = "";
+
+    public MainActivityViewModel(@NonNull Application application) {
+        super(application);
+        this.contexto = application.getApplicationContext();
+    }
+
+    public LiveData<Double> getConvertido(){
+        if(Convertido == null){
+            this.Convertido = new MutableLiveData<>();
+        }
+        return Convertido;
+    }
+
+    public void convertirDollarEuro(String dollares){
+        double valorEuro = 0.93;
+        if(dollares.isEmpty() || dollares=="" || dollares==" "){
+            mensaje= "Debes escribir un número";
+            Toast.makeText(contexto, mensaje,Toast.LENGTH_SHORT).show();
+        }else{
+            double valorDollar = Double.parseDouble(dollares);
+
+            Convertido.setValue(valorDollar*valorEuro);
+        }
+
+    }
+    public void convertirEuroDollar(String euros){
+        double valorDollar= 1.08;
+        if(euros.isEmpty() || euros=="" || euros ==" "){
+            mensaje= "Debes escribir un número";
+            Toast.makeText(contexto, mensaje,Toast.LENGTH_SHORT).show();
+        }else{
+            double valorEuro = Double.parseDouble(euros);
+            Convertido.setValue(valorEuro*valorDollar);
+        }
+
+    }
+
+}
